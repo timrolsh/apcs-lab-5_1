c90dcab60073cb9d87a0027fb5b82a9b81aed546
Krish Arora <43799189+kna27@users.noreply.github.com>
2022-03-21 10:19:14 -0400

    Add bird tests

--- tests/Group3_1BirdConstructor.java
+++ tests/Group3_1BirdConstructor.java
@@ -1,5 +1,15 @@
 package tests;
 
-public class Group3_1BirdConstructor {
-
+import bcatest.BCATestScenario;
+import vehicle.ChevroletBird;
+public class Group3_1BirdConstructor extends BCATestScenario {
+    public int runTest() {
+        ChevroletBird c1 = new ChevroletBird(50);
+        assertFalse(c1.checkWingsExtended(), "Wings should be retracted");
+        assertEquals(c1.toString(), "Chevrolet Bird (50.0 mi)", "Should have 50 miles on constructing");
+        assertEquals(String.valueOf(c1.getMaxRange()), "250", "Max range should be 250");
+        assertEquals(String.valueOf(c1.getRemainingRange()), "250",
+                "Car has never been driven so remainingRange should be maxRange");
+        return getFailedCount();
+    }
 }
